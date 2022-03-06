849fb9eb6527c2f4519a24e916ad4778808b6e11
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-03-06 21:34:59 +0330

    initial commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+#open a cygmin terminal
+#run the first command then run ./run.sh. it will compile and run your code
+cd C:/Users/Muhammad/CLionProjects/untitled2
+gcc src/main.cpp -lglut -lglu -lgl
+export DISPLAY=192.168.163.1:0.0
+./a.exe
