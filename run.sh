ffefdbe4c5e5ca6de83c66388aba8f8fb9e86919
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-04-04 09:52:21 +0430

    learned how to draw circle

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #open a cygmin terminal
 #run the first command then run ./run.sh. it will compile and run your code
 cd C:/Users/Muhammad/CLionProjects/untitled2
-gcc src/main.cpp -lglut -lglu -lgl
-export DISPLAY=192.168.163.1:0.0
+g++ src/main.cpp -lglut -lglu -lgl
+export DISPLAY=192.168.43.228:0.0
 ./a.exe
+# if you ever fail, just open a cmd and type in "ipconfig"
+# then find your current ip address in front of "ipv4"
+# then, replace it with the ip address that is written above.
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,47 @@
-
-#include<cmath>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include "GL/glut.h"
-
-#define pi 3.142857
+#include <GL/glut.h>
+#include <iostream>
+#include <math.h>
 
 using namespace std;
 
-// function to initialize
-void myInit ()
-{
-    // making background color black as first
-    // 3 arguments all are 0.0
+void displayFunction();
+void initialize();
 
-    // making picture color green (in RGB mode), as middle argument is 1.0
-    glColor4f(250,36,100,1.0);
+int main (int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("openGL Learning");
+    glutInitWindowPosition(0,0);
+    glutInitWindowSize(1280,720);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
-    // breadth of picture boundary is 1 pixel
-    glPointSize(1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+    initialize();
 
-    // setting window dimension in X- and Y- direction
-    gluOrtho2D(-780, 780, -420, 420);
+    glutDisplayFunc(displayFunction);
+    glutMainLoop();
 }
 
-void display (void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-    float x, y, i;
-    // iterate y up to 2*pi, i.e., 360 degree
-    // with small increment in angle as
-    // glVertex2i just draws a point on specified co-ordinate
-    for ( i = 0; i < (2 * pi); i += 0.001)
-    {
-        // let 200 is radius of circle and as,
-        // circle is defined as x=r*cos(i) and y=r*sin(i)
-        x = 200 * cos(i);
-        y = 200 * sin(i);
-
-        glVertex2i(x, y);
-    }
+void mkDrawCircle() {
+    double theta;
+    glBegin(GL_POLYGON);
+        for (int i = 0; i < 360; i++) {
+            theta = i * M_PI / 180;
+            glVertex2f(300 + 200 * cos(theta),300 + 1000 * sin(theta));
+        }
     glEnd();
+}
+
+void displayFunction() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0,0.0,0.0);
+    mkDrawCircle();
     glFlush();
 }
 
-int main (int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    // giving window size in X- and Y- direction
-    glutInitWindowSize(1366, 768);
-    glutInitWindowPosition(0, 0);
-
-    // Giving name to window
-    glutCreateWindow("Circle Drawing");
-    myInit();
-
-    glutDisplayFunc(display);
-    glutMainLoop();
+void initialize() {
+    glClearColor(0.5, 1.0, 1.0,0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0,1280.0,720.0,0.0);
 }
